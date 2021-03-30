f1c9437660a03f33a501db5fc5c9c2202ac2950c
kishorvalgen <nandakishor.ks@valgen.com>
2021-03-30 08:54:05 -0400

    Chapter 13

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, {Component} from 'react'
-import './App.css';
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/Message'
-import Counter from './components/Counter'
+import React, { Component } from "react";
+import "./App.css";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
+import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick"
 
-class App extends Component{
-  render(){
+class App extends Component {
+  render() {
     return (
       <div className="App">
-        <Counter/>
-        {/* <Message/> */}
-          {/* <Greet name="NameOne" nikName="One">
+        <FunctionClick/>
+        <ClassClick/>
+        {/*<Counter/>
+        <Message/>
+          <Greet name="NameOne" nikName="One">
             <p>This is child props</p>
           </Greet>
           <Greet name="NameTwo" nikName="Two">
             <button>Click</button>
-          </Greet>*/
+          </Greet>
            <Greet name="NameThree" nikName="Three"/> 
-          /*<Welcome name="NameOne" nikName="One"></Welcome>
-          <Welcome name="NameTwo" nikName="Two"></Welcome>*/}
+          <Welcome name="NameOne" nikName="One"></Welcome>
+          <Welcome name="NameTwo" nikName="Two"></Welcome>
           <Welcome name="NameThree" nikName="Three"></Welcome>
-          <Hello/> 
+          <Hello/>  */}
       </div>
     );
   }
