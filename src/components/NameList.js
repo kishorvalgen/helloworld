f1fc4010781ebef4fd82e587700a493c8f756de8
kishorvalgen <nandakishor.ks@valgen.com>
2021-04-02 05:24:18 -0400

    Chapter 19 - Index as key Anti pattern

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,8 +2,8 @@ import React from "react";
 import Person from "./Person";
 
 function NameList() {
-  const names = ["One", "Two", "Three"];
-  const nameList = names.map((name) => <h2>{name}</h2>);
+  const names = ["One", "Two", "Three", "One"];
+  const nameList = names.map((name, index) => <h2 key={index}>{index}{name}</h2>);
   const persons = [
     {
       id: 1,
@@ -24,9 +24,9 @@ function NameList() {
       skills: ["ReactJS", "HTML", "UI UX"],
     },
   ];
-  const personsList = persons.map(person => <Person person={person} />);
-  //   return <div>{nameList}</div>;
-  return <div>{personsList}</div>;
+  const personsList = persons.map(person => <Person key={person.id} person={person} />);
+     return <div>{nameList}</div>;
+  //return <div>{personsList}</div>;
 }
 
 export default NameList;
