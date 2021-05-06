c9b13691e0a68abcbcda60dee13fb154a0f04987
kishorvalgen <nandakishor.ks@valgen.com>
2021-05-06 07:45:28 -0400

    Chapter - 34 - Higher Order Components (Part 2)

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import UpdatedComponent from "./counterOne";
 
 class ClickCounter extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-             count: 0
-        }
-    }
-    
-    incrementCount = () => {
-        this.setState(prevState => {
-            return { count: prevState.count + 1}
-        })
-    }
-
-    render() {
-        const { count } = this.state
-        return (
-            <div>
-                <button onClick={this.incrementCount}>Click {count} times</button>
-            </div>
-        )
-    }
+  render() {
+    const { count, incrementCount } = this.props;
+    return (
+      <div>
+        <button onClick={incrementCount}>Click {count} times</button>
+      </div>
+    );
+  }
 }
 
-export default ClickCounter
+export default UpdatedComponent(ClickCounter);
